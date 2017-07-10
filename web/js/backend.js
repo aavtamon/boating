4ad59a8be6cb3a5e323394ac2d1534dbd8fd2f49
Avtamonov, Anton <anton.avtamonov@arris.com>
2017-07-09 23:50:35 -0400

    started to work on the location

--- web/js/backend.js
+++ web/js/backend.js
@@ -1,49 +1,50 @@
 Backend = {
-    _reservationContext: {},
-    
-    
-    resetReservationContext: function() {
-      this._reservationContext = {};
-    },
-    getReservationContext: function() {
-      return this._reservationContext;
-    },
-    
-    getCurrentDate: function() {
-      return new Date("9/10/2002");
-    },
-    
-    getSchedulingBeginDate: function() {
-      return new Date("9/11/2002");
-    },
+  _reservationContext: {},
+  
+  
+  resetReservationContext: function() {
+    this._reservationContext = {};
+  },
+  getReservationContext: function() {
+    return this._reservationContext;
+  },
+  
+  getCurrentDate: function() {
+    return new Date("9/10/2002");
+  },
+  
+  getSchedulingBeginDate: function() {
+    return new Date("9/11/2002");
+  },
 
-    getSchedulingEndDate: function() {
-      return new Date("11/12/2002");
-    },
-    
-    getAvailableTimes: function(date) {
-      if (date.getDate() == 19) {
-        return [];
-      }
-      
-      var firstTime = new Date(date);
-      firstTime.setHours(10, 0);
-      
-      var secondTime = new Date(date);
-      secondTime.setHours(13, 0);
-
-      return [{time: firstTime, minDuration: 2, maxDuration: 2, id: 1}, {time: secondTime, minDuration: 1, maxDuration: 2, id: 2}];
-    },
-    
-    getLocationInfo: function(locationId) {
-      if (locationId == 1) {
-        return {name: "Great Marina", address: "1745 Lanier Islands Parkway, Suwanee 30024"};
-      } else if (locationId == 2) {
-        return {name: "Parking lot at the beach", address: "1111 Lanier Islands Parkway, Suwanee 30024"};
-      } else if (locationId == 3) {
-        return {name: "Dam parking", address: "2222 Buford Highway, Cumming 30519"};
-      } else {
-        return null;
-      }
+  getSchedulingEndDate: function() {
+    return new Date("11/12/2002");
+  },
+  
+  getAvailableTimes: function(date) {
+    if (date.getDate() == 19) {
+      return [];
     }
+    
+    var firstTime = new Date(date);
+    firstTime.setHours(10, 0);
+    
+    var secondTime = new Date(date);
+    secondTime.setHours(13, 0);
+
+    return [{time: firstTime, minDuration: 2, maxDuration: 2, id: 1}, {time: secondTime, minDuration: 1, maxDuration: 2, id: 2}];
+  },
+  
+  
+  getCenterLocation: function() {
+    return {lat: 34.2288159, lng: -83.9592255, zoom: 11};
+  },
+  
+  getLocations: function() {
+    return [
+      {id: 1, lat: 34.2169323, lng: -83.9452699, name: "Great Marina", address: "1745 Lanier Islands Parkway, Suwanee 30024"},
+      {id: 2, lat: 34.2305583, lng: -83.9294771, name: "Parking lot at the beach", address: "1111 Lanier Islands Parkway, Suwanee 30024"},
+      {id: 3, lat: 34.2700139, lng: -83.8967458, name: "Dam parking", address: "2222 Buford Highway, Cumming 30519"}
+    ];
+  }
 }
--- web/screens/booking_location.js
+++ web/screens/booking_location.js
@@ -4,18 +4,51 @@ BookingLocation = {
     this._canProceedToNextStep();
   },
     
+  
+  initMap: function() {
+    var centerLocation = Backend.getCenterLocation();
+    var map = new google.maps.Map(document.getElementById("BookingLocation-Screen-SelectionPanel-LocationMap"), {
+      zoom: centerLocation.zoom,
+      center: centerLocation
+    });
+    
+//    map.addListener('center_changed', function() {
+//      setTimeout(function() {
+//        map.panTo(centerLocation);
+//      }, 3000);
+//    });    
+    
+    var locations = Backend.getLocations();
+    for (var i in locations) {
+      var location = locations[i];
+
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        label: location.name,
+        icon: "imgs/boat.png",
+        animation: google.maps.Animation.DROP,
+        _location: location
+      });
+      
+      marker.addListener('click', function(marker) {
+        Backend.getReservationContext().location = marker._location;
+        this._canProceedToNextStep();
+      }.bind(this, marker));
+    }
+  },
+  
+  
   _canProceedToNextStep: function() {
     var reservationContext = Backend.getReservationContext();
     if (reservationContext.date != null && reservationContext.interval != null && reservationContext.duration != null && reservationContext.location != null) {
       $("#BookingLocation-Screen-ButtonsPanel-NextButton").removeAttr("disabled");
       
-      var locationDetails = Backend.getLocationInfo(reservationContext.location);
-      
       var tripDate = reservationContext.date.getMonth() + "/" + reservationContext.date.getDate() + "/" + reservationContext.date.getFullYear();
       var tripTime = reservationContext.interval.time.getHours() + " " + (reservationContext.interval.time.getHours() >= 12 ? 'pm' : 'am');
       var tripDuration = reservationContext.duration + (reservationContext.duration == 1 ? " hour" : " hours");
       var summaryInfo = "You selected " + tripDate + ", " + tripTime + " for " + tripDuration
-                        + ", pick up at " + locationDetails.name;
+                        + ", pick up at " + reservationContext.location.name;
       
       $("#BookingLocation-Screen-ButtonsPanel-Summary").text(summaryInfo);
     } else {
