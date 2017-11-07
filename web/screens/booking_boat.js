805bdb1e63436836895ff608578a58d0730e666c
Avtamonov, Anton <anton.avtamonov@arris.com>
2017-11-06 23:05:10 -0500

    multiple images supported

--- web/screens/booking_boat.js
+++ web/screens/booking_boat.js
@@ -1,4 +1,5 @@
 BookingBoat = {
+  _currentImageIndex: 0,
   
   onLoad: function() {
     if (Backend.isPayedReservation()) {
@@ -11,6 +12,14 @@ BookingBoat = {
     $("#BookingBoat-Screen-Description-NextButton").click(function() {
       Main.loadScreen("booking_time");
     });
+    
+    $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Title-PreviousButton").click(function() {
+      this._showBoatPicture(--this._currentImageIndex);
+    }.bind(this));
+    $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Title-NextButton").click(function() {
+      this._showBoatPicture(++this._currentImageIndex);
+    }.bind(this));
+
     
     this._canProceedToNextStep();
     
@@ -62,11 +71,21 @@ BookingBoat = {
     $("#BookingBoat-Screen-SelectionPanel-BoatDescription-Details-Engine-Value").html(boat.engine);
     $("#BookingBoat-Screen-SelectionPanel-BoatDescription-Details-Mileage-Value").html(boat.mileage + " mpg");
 
-    if (boat.images.length > 0) {
-      var primaryImage = boat.images[0];
-      $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Picture").attr("src", primaryImage.url);
-      $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Note").html(primaryImage.description);
+    this._showBoatPicture(this._currentImageIndex);
+  },
+  
+  _showBoatPicture: function(imageIndex) {
+    var reservationContext = Backend.getReservationContext();
+    var boat = Backend.getBookingConfiguration().locations[reservationContext.location_id].boats[reservationContext.boatId];
+
+    if (imageIndex >= 0 && imageIndex < boat.images.length) {
+      var imgResource = boat.images[imageIndex];
+      $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Picture").attr("src", imgResource.url);
+      $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Note").html(imgResource.description);
     }
+    
+    $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Title-PreviousButton").prop("disabled", imageIndex == 0);
+    $("#BookingBoat-Screen-SelectionPanel-BoatPictures-Title-NextButton").prop("disabled", imageIndex >= boat.images.length - 1);
   },
   
   
