e158eacc23fbe876c974f4b950b97c3a1e398753
Anton Avtamonov <anton.avtamonov@gmail.com>
2021-05-14 23:15:15 -0400

    create db script modified

--- create-database.sh
+++ create-database.sh
@@ -4,9 +4,11 @@ DATABASE='boating'
 DBUSER='boatserver'
 DBPASSWORD='test-password'
 
+
+echo "Creating database ${DATABASE} for the user ${DBUSER} with password ${DBPASSWORD}"
+
 mysql -uroot -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY '${DBPASSWORD}';"
 mysql -uroot -e "CREATE DATABASE ${DATABASE};"
 mysql -uroot -e "GRANT ALL ON ${DATABASE}.* TO '${DBUSER}'@'localhost';"
 
-
-
+echo "All done!"
